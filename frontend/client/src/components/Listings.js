270eb3cb4de230b5fd26cea9349046ca8c6e610a
Yuheng Huang <yuheng08@gmail.com>
2017-10-07 09:50:25 -0700

    added transitions for the listings page

--- frontend/client/src/components/Listings.js
+++ frontend/client/src/components/Listings.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
-import { Card, Container, Icon } from 'semantic-ui-react';
+import { Card, Container, Icon, Transition } from 'semantic-ui-react';
 
 class Listings extends Component {
   images = ['daniel.jpg', 'elliot.jpg', 'matthew.png', 'rachel.png'];
+  state = {visible: false}
+
+  componentDidMount() {
+    this.setState({visible: true})
+  }
 
   render() {
     var { listings } = this.props;
@@ -10,19 +15,21 @@ class Listings extends Component {
     console.log(this.images);
 
     return (
-      <Container style={{marginTop: '20px'}}>
-        <Card.Group itemsPerRow={3}>
-          {listings.map(listing => (
-            <Card 
-              image={'/' + this.images[Math.floor(Math.random() * this.images.length)]}
-              header={listing.posted_by}
-              meta={listing.activity}
-              description={`Schedule on ${listing.scheduled} for ${listing.duration} hours`}
-              extra={(<a><Icon name='user' /> {listing.buddies} {listing.buddies === 1 ? 'buddy' : 'buddies'} </a>)}
-            />
-          ))}
-        </Card.Group>
-      </Container>
+      <Transition visible={this.state.visible} duration={1000}>
+        <Container style={{marginTop: '20px'}}>
+          <Card.Group itemsPerRow={3}>
+            {listings.map(listing => (
+              <Card 
+                image={'/' + this.images[Math.floor(Math.random() * this.images.length)]}
+                header={listing.posted_by}
+                meta={listing.activity}
+                description={`Schedule on ${listing.scheduled} for ${listing.duration} hours`}
+                extra={(<a><Icon name='user' /> {listing.buddies} {listing.buddies === 1 ? 'buddy' : 'buddies'} </a>)}
+              />
+            ))}
+            </Card.Group>
+        </Container>
+      </Transition>
     )
   }
 }
