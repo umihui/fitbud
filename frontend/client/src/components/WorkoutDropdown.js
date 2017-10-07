c292151767e057b4f44400a60b4d85789f9a2c6d
Tirumari <tirumari@gmail.com>
2017-10-07 15:45:30 -0700

    further tweaking of the dashboard

---
+++ frontend/client/src/components/WorkoutDropdown.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Card, Icon, Image } from 'semantic-ui-react';
+
+class WorkoutDropdown extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <a><Icon name='user' /> {this.props.buddies} {this.props.buddies === 1 ? 'buddy' : 'buddies'} </a>
+      </div>
+    );
+  }
+}
+
+export default WorkoutDropdown;
