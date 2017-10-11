13be0d629677c52e0636412d1ff10f79e6720d97
Albert Chung <aychung86 (at) gmail.com>
2017-10-11 11:43:10 -0700

    (feat) file upload component now use semantic ui

--- client/src/components/Upload.jsx
+++ client/src/components/Upload.jsx
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
-
+import { Input, Icon } from 'semantic-ui-react';
 
 class Upload extends Component {
   constructor(props) {
     super(props);
     this.state = {
       file: '',
+      fname: 'Select File',
     }
+    this.uploadFile = this.uploadFile.bind(this)
   }
 
   uploadFile (inf = this.props.inf, path= this.props.path) {
-    console.log('initiate upload',this.state.file);
-
+    // console.log('initiate upload',this.state.file);
     let data = new FormData();
 
     for (let key in this.props.inf) {
@@ -19,18 +20,6 @@ class Upload extends Component {
     }
 
     data.append('file', this.state.file, this.state.file.name);
-
-    // Axios.post(path, data)
-    //   .then(res => {
-    //     console.log('file upload complete');
-    //     // reload file list
-    //     return Axios.get('/file', {params: {roomname: this.props.room.roomname}})
-    //   }).catch(err => {
-    //     console.log('failed to upload', err);
-    //   }).then(res => {
-    //     console.log(res.data, this.props.room.roomname);
-    //     this.props.updateFiles(res.data);
-    //   });
 
     var options = {
       method: 'POST',
@@ -47,16 +36,28 @@ class Upload extends Component {
   }
 
   setFile(event) {
-    // console.log('setFile', event.target.files);
+    var fname = event.target.files[0] ? event.target.files[0].name : 'Select File';
+
     this.setState({
       file: event.target.files[0],
+      fname: fname,
     });
   }
 
+  selectFile(event, data) {
+    this.inputElement.click();
+  }
+
+  upload(event) {
+    this.uploadFile();
+  }
+
   render() {
+
     return (
       <div>
-        <input type="file" onChange={this.setFile.bind(this)}/> <button onClick={this.uploadFile.bind(this)}>upload</button>
+        <Input onClick={this.selectFile.bind(this)} readOnly placeholder={this.state.fname} action={{icon: 'upload', onClick:this.upload.bind(this)}}/>
+        <input ref={input => this.inputElement = input} id="fileInput" onClick={this.testing} style={{visibility: 'hidden'}} type="file" onChange={this.setFile.bind(this)} />
       </div>
     );
   }
@@ -65,5 +66,6 @@ class Upload extends Component {
 
 export default Upload;
 
+// <input type="file" onChange={this.setFile.bind(this)}/> <button onClick={this.uploadFile.bind(this)}>upload</button>
 
-
+// 
